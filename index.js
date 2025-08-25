7145c55d2206280d09389c7c229c2604a1a94be0
lovexd339 <rajputhassan6789@gmail.com>
2025-08-25 16:57:19 +0530

    Update and rename bot.js to index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+const login = require('fca-unofficial');
+const fs = require('fs');
+const config = require('./config.json');
+const messageHandler = require('./handlers/messageHandler');
+
+const appState = require('./appstate.json');
+
+login({ appState }, (err, api) => {
+  if (err) return console.error('Login Failed:', err);
+
+  api.setOptions({
+    forceLogin: true,
+    listenEvents: true,
+    selfListen: false,
+    logLevel: "silent"
+  });
+
+  console.log('🤖 Bot is now online!');
+
+  api.listenMqtt((err, event) => {
+    if (err) return console.error(err);
+    messageHandler(api, event);
+  });
+});
